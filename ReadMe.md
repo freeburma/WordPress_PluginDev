a8fae2cfe65e6b3622d8cd57d82e4566e2b1805f
freeburma <kophyoblue@gmail.com>
2021-05-26 16:55:54 +1200

    Generating Data with PHP

--- ReadMe.md
+++ ReadMe.md
@@ -3,4 +3,9 @@
 ### Git Branches 
 1. 01-Plugin-Template
 2. 02-Google-Chart-Frontend
+   -> 02-Google-Chart-Frontend-01
+
+
+
+
 
--- myplugin/views/GoogleChart.php
+++ myplugin/views/GoogleChart.php
@@ -1,5 +1,6 @@
 <h1>Welcome to my plugin</h1>
 
+<!-- Ref: https://developers.google.com/chart/interactive/docs/gallery/linechart#overview -->
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <div id="chart_div"></div>
 
@@ -13,14 +14,43 @@ function drawBackgroundColor() {
       data.addColumn('number', 'Monthly Sale');
 
       data.addRows([
-        ['Jan', 3000], ['Feb', 3134],  
-        ['Mar', 4199], ['Apl', 1999], 
-        ['May', 700], ['Jun', 300], 
-        ['Jul', 450], ['Aug', 1024], 
-        ['Sep', 2456], ['Oct', 4579], 
-        ['Nov', 5104], ['Dec', 5456], 
+        // ['Jan', 3000], ['Feb', 3134],  
+        // ['Mar', 4199], ['Apl', 1999], 
+        // ['May', 700], ['Jun', 300], 
+        // ['Jul', 450], ['Aug', 1024], 
+        // ['Sep', 2456], ['Oct', 4579], 
+        // ['Nov', 5104], ['Dec', 5456], 
+
+        //// Adding data with php 
+        <?php 
+        $saleData = array(
+            array('Jan', 3000), 
+            array('Feb', 3134),  
+            array('Mar', 4199), 
+            array('Apl', 1999), 
+            array('May', 700), 
+            array('Jun', 300), 
+            array('Jul', 450), 
+            array('Aug', 1024), 
+            array('Sep', 2456), 
+            array('Oct', 4579), 
+            array('Nov', 5104), 
+            array('Dec', 5456), 
+            array('Jan', 200), 
+        ); 
+
+
+        foreach ($saleData as $sale)
+        {
+            echo ("['". $sale[0] . "'," . $sale[1] . "], "); 
+        }// end foreach 
+
+
+      ?>
+      
       ]);
 
+      
 
       var options = {
         
@@ -37,4 +67,27 @@ function drawBackgroundColor() {
       chart.draw(data, options);
     }
 </script>
-      
+<!-- <?php 
+        $saleData = array(
+            array('Jan', 3000), 
+            array('Feb', 3134),  
+            array('Mar', 4199), 
+            array('Apl', 1999), 
+            array('May', 700), 
+            array('Jun', 300), 
+            array('Jul', 450), 
+            array('Aug', 1024), 
+            array('Sep', 2456), 
+            array('Oct', 4579), 
+            array('Nov', 5104), 
+            array('Dec', 5456), 
+        ); 
+
+
+        foreach ($saleData as $sale)
+        {
+            echo ("['". $sale[0] . "'," . $sale[1] . "], "); 
+        }// end foreach 
+
+
+      ?> -->
