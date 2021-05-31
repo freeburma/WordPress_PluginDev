64da61bd6018f0f7b03de550860f808910662c39
freeburma <kophyoblue@gmail.com>
2021-06-01 03:09:01 +1200

    Activate/Deactivate plugin

--- myplugin/include/PluginActivate.php
+++ myplugin/include/PluginActivate.php
@@ -3,6 +3,36 @@ class PluginActivate
 {
     public static function activate()
     {
+        //// CREATING the table after we installed the plugin
+        global $wpdb; 
+
+        $createTable = $wpdb->prepare("CREATE TABLE IF NOT EXISTS wp_Custom_SaleData(
+                                        Id INT NOT NULL AUTO_INCREMENT, 
+                                        Month VARCHAR(30), 
+                                        SaleAmount DECIMAL(16, 2),
+                                        PRIMARY KEY (ID) 
+                                    )"); 
+        $wpdb->query($createTable); 
+
+        $addData = $wpdb->prepare("INSERT INTO wp_Custom_SaleData
+                                                    (Month, SaleAmount) 
+                                                    VALUES 
+                                                    ('Jan', 3000), 
+                                                    ('Feb', 3134),  
+                                                    ('Mar', 4199), 
+                                                    ('Apl', 1999), 
+                                                    ('May', 700), 
+                                                    ('Jun', 300), 
+                                                    ('Jul', 450), 
+                                                    ('Aug', 1024), 
+                                                    ('Sep', 2456),
+                                                    ('Oct', 4579), 
+                                                    ('Nov', 5104), 
+                                                    ('Dec', 5456)
+                                        "); 
+
+        $wpdb->query($addData); 
+
         flush_rewrite_rules( );
 
     }// end function deactivate()
--- myplugin/include/PluginDeactivate.php
+++ myplugin/include/PluginDeactivate.php
@@ -4,6 +4,12 @@ class PluginDeactivate
 {
     public static function deactivate()
     {
+        //// DELETING the table after we installed the plugin
+        global $wpdb; 
+
+        $dropTable = $wpdb->prepare("DROP TABLE IF EXISTS wp_Custom_SaleData"); 
+        $wpdb->query($dropTable); 
+
         flush_rewrite_rules( );
 
     }// end function deactivate()
